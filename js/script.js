89a39fdd9eaf8654c4839c091a6db308cb887995
Callaghanjake <jake_2024@hotmail.co.uk>
2024-08-08 13:43:49 +0100

    feat: add additional button variables to JS and connected to stop and reset functions.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ let secondsElapsed = 0;
 let interval = null;
 let time = document.querySelector('.time');
 let startBtn = document.querySelector('.startBtn');
+let stopBtn = document.querySelector('.stopBtn');
+let resetBtn = document.querySelector('.resetBtn');
 
 function leadZero(value) {
     return String(value).padStart(2, "0");
@@ -32,8 +34,16 @@ function stopClock () {
     clearInterval(interval);
 }
 
+stopBtn.addEventListener('click', () => {
+    stopClock();
+});
+
 function resetClock () {
     stopClock();
     secondsElapsed = 0;
     setTime();
 }
+
+resetBtn.addEventListener('click', () => {
+    resetClock();
+});
